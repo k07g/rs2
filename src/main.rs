3f8c24611d1c20e93721c201286f3438908a7744
k07g <155704074+k07g@users.noreply.github.com>
2026-03-09 22:00:55 +0900

    ランダム生成を追加 (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use rand::RngExt;
+
+
 fn main() {
-    println!("Hello, world!");
+    let op1 = rand::rng().random_range(0..100);
+    let op2 = rand::rng().random_range(0..100);
+    println!("{} + {} = ??", op1, op2);
+    println!("??の値を入力して下さい:");
+    let mut ans_input = String::new();
+    std::io::stdin().read_line(&mut ans_input).unwrap();
+    let ans_input = ans_input.trim().parse::<i32>().unwrap();
+    dbg!(ans_input);
+    if dbg!(ans_input == op1 + op2) {
+        println!("正解！");
+    } else {
+        println!("不正解！");
+    }
 }
